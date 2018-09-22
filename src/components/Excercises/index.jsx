95978e06aedb335e981fbaab3133c89ae1047055
Nithin Ramakrishna <nithinrk@outlook.com>
2018-09-22 09:45:59 +0100

    Grid layout

--- src/components/Excercises/index.jsx
+++ src/components/Excercises/index.jsx
@@ -1,9 +1,38 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
 
-export default props => (
-  <Grid container>
-    <Grid Item>Left Pane</Grid>
-    <Grid Item>Right Pane</Grid>
-  </Grid>
-);
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+});
+
+const Excercises = props => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>Left Pane</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>Right Pane</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+Excercises.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Excercises);
